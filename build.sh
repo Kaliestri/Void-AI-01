0c8a105001aef02becc16c84d0178afbbb0e991c
Kaliestri <parrafranklinson1666@gmail.com>
2025-11-21 00:53:38 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
+set -e
+
+# Instalar Python 3.10.12 usando pyenv (esto puede ser lento)
+PYTHON_VERSION="3.10.12"
+pyenv install -s $PYTHON_VERSION
+pyenv global $PYTHON_VERSION
+
 pip install --upgrade pip
-pip install --only-binary=all -r requirements.txt
+pip install -r requirements.txt
